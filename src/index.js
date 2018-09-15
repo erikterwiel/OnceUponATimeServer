cbde25c19c7eac838139b6db4bc579a318099ae9
ehterwie <ehterwie@uwaterloo.ca>
2018-09-15 07:00:03 -0400

    Got socketio server working

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket.listen(server);
+
+const connections = [];
+
+server.listen(process.env.PORT || 5000);
+
+io.sockets.on("connection", (socket) => {
+  connections.push(socket);
+  console.log(`Connected: ${connections.length} sockets connected`);
+
+  socket.on("disconnect", () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnected: ${connections.length} sockets connected`);
+  });
+
+  socket.on("sendCommand", (command) => {
+    io.sockets.emit("newCommand", { command })
+  })
+});
+
+console.log("AidsServer running on port 5000");
